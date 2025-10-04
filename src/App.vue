93c95b7ba7c5c97dca2c8d46a562d7ebb8d35edf
Felipe Alvarado <devfelipe663@gmail.com>
2025-10-03 22:43:14 -0500

    feat: add project

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+</script>
+
+<template>
+  <div>
+    <figure>
+      <img src="" alt="">
+    </figure>
+    <h1 class="title">Welcome to pokedex</h1>
+  </div>
+</template>
+
+<style scoped>
+
+.title {
+  font-weight: bold;
+  font-size: 1.625rem;
+  line-height: 1.95rem;
+  color: var(--color-dark);
+}
+</style>
