69ce8271ff8eecef0e86bfc7f73eca7ae077d0f9
Felipe Alvarado <devfelipe663@gmail.com>
2025-10-07 13:54:02 -0500

    feat: add ItemsNotFound component and update layout in HomeView and PokemonList

---
+++ src/components/ItemsNotFound.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h3>Uh-oh!</h3>
+        <p>You look lost on your journey!</p>
+
+        <router-link to="/" class="text-red-10 underline">
+            go back home
+        </router-link>
+    </div>
+</template>
